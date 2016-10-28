6178301debd5fd81086775cd4a71f21386f48986
mrunal4 <mrunal4888@gmail.com>
2016-10-28 15:29:14 +0530

    /usr/bin/bash instead of /bin/bash in rsync-update.sh

--- scripts/rsync-update.sh
+++ scripts/rsync-update.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-#Mrunal : Copy files to respective locations 
+#Mrunal : Take git pull
 echo -e "Process started"
-#docker cp Create-Server-symlink-base-backup-files-dir.sh rsync-testing:/home/docker/code/scripts/
-#docker cp backup.sh rsync-testing:/home/docker/code/scripts/
 
 # Git pull docker code
 docker exec -it gstudio /bin/sh -c "cd /home/docker/code/ && git pull https://github.com/mrunal4/gstudio-docker.git"
@@ -11,7 +9,6 @@ docker exec -it gstudio /bin/sh -c "cd /home/docker/code/ && git pull https://gi
 # Git pull gstudio app code
 docker exec -it gstudio /bin/sh -c "cd /home/docker/code/gstudio/ && git pull https://github.com/gnowledge/gstudio.git"
 
-#docker exec -it gstudio /bin/sh -c "cd /home/docker/code/gstudio/gnowsys-ndf && /usr/bin/python /home/docker/code/gstudio/gnowsys-ndf/manage.py fillCounter"
 echo -e "Process ended"
 
 
