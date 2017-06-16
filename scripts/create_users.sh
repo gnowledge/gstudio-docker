0ee4af14f2f5b2025ca9652b3fa3a04db4e4d754
mrunal4 <mrunal4888@gmail.com>
2017-06-16 10:54:39 +0530

    mizoram installer related scripts (files from the installer sent to mizoram)

---
+++ scripts/create_users.sh
@@ -0,0 +1,1 @@
+docker exec -it gstudio /bin/sh -c "/usr/bin/python /home/docker/code/gstudio/gnowsys-ndf/manage.py sync_users /home/docker/code/user-csvs/sp100_users.csv"
--- scripts/install-to-disk.sh
+++ scripts/install-to-disk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Following variables are used to store the color codes for displaying the content on terminal
 # red="\033[0;31m" ;
 # green="\033[0;32m" ;
@@ -19,28 +20,29 @@ grey="\033[0;97m" ;
 white="\033[0;98m" ;
 reset="\033[0m" ;
 
+sudo dmesg -n1
 
 # Mrunal : below part is added by Mrunal as suggested by Nagarjuna
-echo -e "\n${cyan}***   Learning Studio - DOER Edition (April 2016)   ***${reset}\n" ;
-echo -e "${cyan}      school server installation      ${reset}\n" ;
+echo -e "\n\n\n${cyan}***   CLIx Server Installer - Release Version April 2017   ***${reset}\n\n" ;
+echo -e "${cyan}      School server installation      ${reset}\n\n" ;
 echo -e "\n${brown}Note : \nThis installation is a one-time or infrequently used process.${reset}" ;
-echo -e "${brown}It uses a terminal-console which may show many details during installations.${reset}" ;
+echo -e "${brown}It uses a terminal-console which may show many details during installation.${reset}" ;
 echo -e "${brown}This is normal. Please let the installation proceed.${reset}\n" ;
 sleep 5
-docker start webserver
-echo -e "\n"
-echo -e "\n"
 
+echo -e "\n${cyan}Starting clix ${reset}";
+docker start gstudio
+echo -e "\n\n\n";
 sleep 5
 
 echo -e "\n${cyan}Name the disk where do you want to install the server? ${reset}" ;
-echo -e "${brown}(For example 'sda' or 'sdb' or 'sdc') ${reset}"
+echo -e "${brown}(For example 'sda' or 'sdb' or 'sdc') ${reset}" ;
 echo -e "${brown}{if you are not sure and want to exit simply type enter} ${reset}" ;
 check_disk_h=`lsblk | grep SIZE`
 check_disk_d=`lsblk | grep disk`
-echo -e "\n${purple}$check_disk_h ${reset}"
-echo -e "${blue}$check_disk_d ${reset}\n"
-echo -e -n "${cyan}disk name : ${reset}"
+echo -e "\n${purple}$check_disk_h ${reset}" ;
+echo -e "${blue}$check_disk_d ${reset}\n" ;
+echo -e -n "${cyan}disk name : ${reset}" ;
 
 read disk_i ;
 
@@ -54,8 +56,8 @@ else
     echo -e "\n${green}Disk entered is $disk_i ${reset}" ;
     check_disk=`lsblk | grep $disk_i | grep disk | wc -l`
     if [[ "$check_disk" != "1" ]]; then
-	echo -e "\n${red}Invalid input. Hence exiting. Please try again later. ${reset}" ;
-	exit
+    	echo -e "\n${red}Invalid input. Hence exiting. Please try again later. ${reset}" ;
+    	exit
     fi
 
     echo -e "\n${red}Caution: \nIt will format $disk_i disk ${reset}";
@@ -65,30 +67,27 @@ else
     read part_format_i
     if [[ "$part_format_i" == "" ]]; then
 
-	echo -e "\n${brown}No input. Hence exiting. Please try again later. ${reset}" ;
-	exit
+    	echo -e "\n${brown}No input. Hence exiting. Please try again later. ${reset}" ;
+    	exit
 
     elif  [[ "$part_format_i" == "n" ]] || [[ "$part_format_i" == "N" ]] || [[ "$part_format_i" == "no" ]] || [[ "$part_format_i" == "No" ]] || [[ "$part_format_i" == "NO" ]]; then
 
-	echo -e "\n${red}Invalid input is '$part_format_i'. Hence exiting. Thank you. ${reset}" ;
-	exit
+    	echo -e "\n${red}Invalid input is '$part_format_i'. Hence exiting. Thank you. ${reset}" ;
+    	exit
 
     elif  [[ "$part_format_i" == "y" ]] || [[ "$part_format_i" == "Y" ]] || [[ "$part_format_i" == "yes" ]] || [[ "$part_format_i" == "Yes" ]] || [[ "$part_format_i" == "YES" ]]; then
 
-	echo -e "\n${green}Input is '$part_format_i'. Continuing the process. ${reset}" ;
+	   echo -e "\n${green}Input is '$part_format_i'. Continuing the process. ${reset}" ;
 
     else
 
-	echo -e "\n${red}Input is '$part_format_i'. Oops!!! Something went wrong. ${reset}"
+	   echo -e "\n${red}Input is '$part_format_i'. Oops!!! Something went wrong. ${reset}"
 
     fi
     
 fi
 
-echo -e "\n${cyan}Installing coreos, the host operating system to /dev/$disk_i ${reset}"
-
-sudo /home/core/setup-software/coreos/coreos-install.sh -d /dev/$disk_i -C stable -c /home/core/setup-software/coreos/cloud-config.yaml -b http://localhost/softwares/coreos/mirror 
-
-echo -e "\n${cyan}Successfully installed coreos, the host operating system to /dev/$disk_i ${reset}"
+echo -e "\n${cyan}Installing coreos, the host operating system to /dev/$disk_i ${reset}\n"
+sudo /home/core/setup-software/coreos/coreos-install -d /dev/$disk_i -C stable -c /home/core/setup-software/coreos/cloud-config.yaml -V 1010.5.0 -b http://localhost/softwares/coreos/mirror 
 
 exit
